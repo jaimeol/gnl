4c79d11306547d3976d549c0fb07efd96400e629
jaimeol <142798095+jaimeol@users.noreply.github.com>
2024-07-11 22:35:21 +0200

    Update get_next_line_bonus.c

--- bonus/get_next_line_bonus.c
+++ bonus/get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*ft_free(char *buffer, char *buf)
 {
